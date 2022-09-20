e58c5534527d4bca9d051c2643c59fc595feb9d4
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-09-20 16:49:06 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,51 @@
 ;(function ($) {
-    'use strict';
-    let windowWidth = $(window).width();
+	'use strict';
+	let windowWidth = $(window).width();
 
-    let initSliderBanner = function () {
-        if($('#slider-banner').length) {
+	let initSliderBanner = function () {
+		if ($('#slider-banner').length) {
+			new Swiper('#slider-banner .swiper', {
+				speed: 1000,
+				autoplay: {
+					delay: 8000,
+					disableOnInteraction: false,
+				},
+				loop: 1,
+				effect: 'fade'
+			});
+		}
+	}
 
-        }
-    }
+	let initSliderTestimonials = function () {
+		if ($('#slider-testimonials').length) {
+			new Swiper('#slider-testimonials .swiper', {
+				spaceBetween: 15,
+				speed: 1000,
+				loop: !1,
+				breakpoints: {
+					320: {
+						slidesPerView: 1,
+					},
+					375: {
+						slidesPerView: 1.5,
+					},
+					768: {
+						slidesPerView: 2.5,
+					},
+					1499: {
+						slidesPerView: 3,
+					}
+				},
+				pagination: {
+					el: "#slider-testimonials .swiper-pagination",
+					clickable: 1,
+				}
+			});
+		}
+	}
 
-    $(function () {
-    });
+	$(function () {
+		initSliderBanner();
+		initSliderTestimonials();
+	});
 })(jQuery);
