c553ee5731f0b88e64f2cc4d3149c30856cc5235
potato187 <dhtai97@gmail.com>
2022-11-05 17:40:42 +0700

    responsive header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,225 +1,284 @@
 (function ($) {
-	'use strict';
-	let windowWidth = $(window).width();
+    'use strict';
+    const windowWidth = $(window).width();
 
-	let initSliderBanner = function () {
-		if ($('#slider-banner').length) {
-			new Swiper('#slider-banner .swiper', {
-				speed: 1000,
-				autoplay: {
-					delay: 8000,
-					disableOnInteraction: false,
-				},
-				loop: 1,
-				effect: 'fade',
-			});
-		}
-	};
+    const handleMenuMobile = function (currentWindow) {
+        if (currentWindow >= 1200) return false;
 
-	let initSliderTestimonials = function () {
-		if ($('#slider-testimonials').length) {
-			new Swiper('#slider-testimonials .swiper', {
-				spaceBetween: 15,
-				speed: 1000,
-				loop: !1,
-				breakpoints: {
-					320: {
-						slidesPerView: 1,
-					},
-					375: {
-						slidesPerView: 1.5,
-					},
-					768: {
-						slidesPerView: 2.5,
-					},
-					1499: {
-						slidesPerView: 3,
-					},
-				},
-				pagination: {
-					el: '#slider-testimonials .swiper-pagination',
-					clickable: 1,
-				},
-			});
-		}
-	};
+        const btnToggle = $('#header .btn-show-nav, #header .btn-close-nav');
+        const Body = $('body');
+		const headerNavigation = $('#header .header-wrapper .header-navigation');
 
-	let initSliderNews = function () {
-		if (!$('#slider-news').length) return false;
-		new Swiper('#slider-news > .swiper', {
-			slidesPerView: 4,
-			spaceBetween: 15,
-			speed: 1000,
-			loop: !1,
-			pagination: {
-				el: '#slider-news > .swiper-actions > .swiper-pagination',
-				clickable: 1,
-			},
-			breakpoints: {
-				320: {
-					slidesPerView: 1,
-				},
-				375: {
-					slidesPerView: 1.25,
-				},
-				768: {
-					slidesPerView: 2.5,
-				},
-				1200: {
-					slidesPerView: 3,
-				},
-			},
-		});
-	};
+        headerNavigation.find('ul > li > ul').each(function (index) {
+            $(this).prev().attr({
+                "href": "#subMenu" + index,
+                "data-toggle": "collapse",
+                "aria-expanded" : false,
+                "aria-controls": "subMenu" + index,
+            });
+            $(this).attr({
+                "id": "subMenu" + index,
+                "class": "collapse list-unstyled mb-0",
+            });
 
-	const addPaddingSinglePage = function () {
-		const container = $('.page');
-		const header = $('#header');
-		if (!container.length || !header.length) return false;
-		container.css({ 'padding-top': `${header.outerHeight(true)}px` });
-	};
+            if ($(this).find('ul').length > 0) {
+                $(this).find('ul').each(function (index, element) {
+                    $(element).prev().attr({
+                        "href": "#subMenu_child" + index,
+                        "data-toggle": "collapse"
+                    });
+                    $(element).attr({
+                        "id": "subMenu_child" + index,
+                        "class": "collapse list-unstyled mb-0",
+                    });
+                });
+            }
+        });
 
-	const handleWindowResize = function () {
-		$(window).resize(function () {
-			addPaddingSinglePage();
-		});
-	};
+        headerNavigation.find('ul > li > a').on('click',function () {
+            if ($(this).next('ul').hasClass('show')) {
+                let _ul = $(this).next('ul');
+                _ul.find('ul.show').removeClass('show');
+                _ul.find('a[aria-expanded]').attr('aria-expanded', false);
+            } else {
+                $(this).closest('#hasMenu').find('.show').removeClass('show');
+                $(this).closest('#hasMenu').find('a[aria-expanded]').attr('aria-expanded', false);
+            }
+        });
 
-	const handleZoomImageProduct = function (elm, avatarPhoto, avatarThumb) {
-		if (!elm || !avatarPhoto || !avatarThumb) return false;
-		let i = 0;
-		elm.click(function () {
-			i = 0;
-		});
+        btnToggle.on('click', function () {
+            Body.toggleClass('nav-is-show');
+        });
 
-		elm.fancybox({
-			touch: true,
-			beforeShow: function (instance, current) {
-				let index = $(`[data-fancybox='preview-box'][href='${current.src}']`).attr('data-index');
-				avatarPhoto.slideTo(index - 1);
-				if ($('#product-preview').length > 0) {
-					avatarThumb.slideTo(index - 1);
-				}
-			},
-		});
-	};
+        $(document).on('click', function (event) {
+            if(!$('#header .header-wrapper')[0].contains(event.target)){
+                Body.removeClass('nav-is-show');
+            }
+        });
+    }
 
-	const handleSlideProduct = function () {
-		let [avatarThumb, avatarPhoto] = [];
+    const initSliderBanner = function () {
+        if ($('#slider-banner').length) {
+            new Swiper('#slider-banner .swiper', {
+                speed: 1000,
+                autoplay: {
+                    delay: 8000,
+                    disableOnInteraction: false,
+                },
+                loop: 1,
+                effect: 'fade',
+            });
+        }
+    };
 
-		if (!$('#product-preview').length && $('#product-thumbs').length > 0) {
-			avatarPhoto = new Swiper('#product-thumbs > .swiper', {
-				slidesPerView: 1,
-				navigation: {
-					nextEl: '#button-next',
-					prevEl: '#button-prev',
-				},
-			});
-			return false;
-		}
+    const initSliderTestimonials = function () {
+        if ($('#slider-testimonials').length) {
+            new Swiper('#slider-testimonials .swiper', {
+                spaceBetween: 15,
+                speed: 1000,
+                loop: !1,
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                    },
+                    375: {
+                        slidesPerView: 1.5,
+                    },
+                    768: {
+                        slidesPerView: 2.5,
+                    },
+                    1499: {
+                        slidesPerView: 3,
+                    },
+                },
+                pagination: {
+                    el: '#slider-testimonials .swiper-pagination',
+                    clickable: 1,
+                },
+            });
+        }
+    };
 
-		if (!$('#product-preview').length && !$('#product-thumbs').length) {
-			return false;
-		}
+    const initSliderNews = function () {
+        if (!$('#slider-news').length) return false;
+        new Swiper('#slider-news > .swiper', {
+            slidesPerView: 4,
+            spaceBetween: 15,
+            speed: 1000,
+            loop: !1,
+            pagination: {
+                el: '#slider-news > .swiper-actions > .swiper-pagination',
+                clickable: 1,
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                375: {
+                    slidesPerView: 1.25,
+                },
+                768: {
+                    slidesPerView: 2.5,
+                },
+                1200: {
+                    slidesPerView: 3,
+                },
+            },
+        });
+    };
 
-		avatarThumb = new Swiper('#product-thumbs > .swiper', {
-			loopAdditionalSlides: 0,
-			spaceBetween: 10,
-			slidesPerView: 3,
-			breakpoints: {
-				320: {
-					slidesPerView: 2.5,
-				},
-				525: {
-					slidesPerView: 3.5,
-				},
-				991: {
-					slidesPerView: 4,
-				},
-			},
-		});
-		avatarPhoto = new Swiper('#product-preview > .swiper', {
-			thumbs: {
-				swiper: avatarThumb,
-			},
-			slidesPerView: 1,
-			navigation: {
-				nextEl: '#product-preview .swiper-button.next',
-				prevEl: '#product-preview .swiper-button.prev',
-			},
-		});
+    const addPaddingSinglePage = function () {
+        const container = $('.page');
+        const header = $('#header');
+        if (!container.length || !header.length) return false;
+        container.css({'padding-top': `${header.outerHeight(true)}px`});
+    };
 
-		handleZoomImageProduct($('#product-preview [data-fancybox="preview-box"]'), avatarPhoto, avatarThumb);
-	};
+    const handleWindowResize = function () {
+        $(window).resize(function () {
+            addPaddingSinglePage();
+        });
+    };
 
-	const handleToggleCartList = function () {
-		const btnToggle = $('#toggle-cart-list');
-		const headerCart = $('.header .header-cart');
+    const handleZoomImageProduct = function (elm, avatarPhoto, avatarThumb) {
+        if (!elm || !avatarPhoto || !avatarThumb) return false;
+        let i = 0;
+        elm.click(function () {
+            i = 0;
+        });
 
-		btnToggle.on('click', function () {
-			headerCart.toggleClass('show');
-		});
+        elm.fancybox({
+            touch: true,
+            beforeShow: function (instance, current) {
+                let index = $(`[data-fancybox='preview-box'][href='${current.src}']`).attr('data-index');
+                avatarPhoto.slideTo(index - 1);
+                if ($('#product-preview').length > 0) {
+                    avatarThumb.slideTo(index - 1);
+                }
+            },
+        });
+    };
 
-		$(document).on('click', function (event) {
-			if(!headerCart[0].contains(event.target)) {
-				headerCart.removeClass('show');
-			}
-		});
-	};
+    const handleSlideProduct = function () {
+        let [avatarThumb, avatarPhoto] = [];
 
-	const initSwiperBookingPreview = function () {
-		if ($('#booking-preview').length) {
-			new Swiper('#booking-preview .swiper', {
-				speed: 1000,
-				autoplay: {
-					delay: 8000,
-					disableOnInteraction: false,
-				},
-				loop: 1,
-				navigation: {
-					nextEl: '#booking-preview .swiper-button.next',
-					prevEl: '#booking-preview .swiper-button.prev',
-				},
-			});
-		}
-	};
+        if (!$('#product-preview').length && $('#product-thumbs').length > 0) {
+            avatarPhoto = new Swiper('#product-thumbs > .swiper', {
+                slidesPerView: 1,
+                navigation: {
+                    nextEl: '#button-next',
+                    prevEl: '#button-prev',
+                },
+            });
+            return false;
+        }
 
-	const showModalSearchUser = function () {
-		$('.checkout-guests .guest:not(:first)').on('click', function () {
-			$('#modal-add-user').modal('show');
-		});
-	};
+        if (!$('#product-preview').length && !$('#product-thumbs').length) {
+            return false;
+        }
 
-	const handleDatePicker = function (selectorId) {
-		if(!$(selectorId).length) return false;
-		$(selectorId).flatpickr({
-			dateFormat: 'd-m-Y',
-			defaultDate: 'today',
-			enable: [
-				{
-					from: 'today',
-					to: new Date().fp_incr(60)
-				}
-			]
-		});
-	}
+        avatarThumb = new Swiper('#product-thumbs > .swiper', {
+            loopAdditionalSlides: 0,
+            spaceBetween: 10,
+            slidesPerView: 3,
+            breakpoints: {
+                320: {
+                    slidesPerView: 2.5,
+                },
+                525: {
+                    slidesPerView: 3.5,
+                },
+                991: {
+                    slidesPerView: 4,
+                },
+            },
+        });
+        avatarPhoto = new Swiper('#product-preview > .swiper', {
+            thumbs: {
+                swiper: avatarThumb,
+            },
+            slidesPerView: 1,
+            navigation: {
+                nextEl: '#product-preview .swiper-button.next',
+                prevEl: '#product-preview .swiper-button.prev',
+            },
+        });
 
-	$(function () {
-		initSliderBanner();
-		initSliderTestimonials();
-		initSliderNews();
-		addPaddingSinglePage();
-		handleWindowResize();
+        handleZoomImageProduct($('#product-preview [data-fancybox="preview-box"]'), avatarPhoto, avatarThumb);
+    };
 
-		// PAGE PRODUCT DETAIL
-		handleSlideProduct();
-		handleToggleCartList();
+    const handleToggleCartList = function () {
+        const btnToggle = $('#toggle-cart-list');
+        const headerCart = $('.header .header-cart');
 
-		// PAGE BOOKING GOLF
-		initSwiperBookingPreview();
-		showModalSearchUser();
+        btnToggle.on('click', function () {
+            headerCart.toggleClass('show');
+        });
 
-		handleDatePicker('#date-picker');
+        $(document).on('click', function (event) {
+            if (!headerCart[0].contains(event.target)) {
+                headerCart.removeClass('show');
+            }
+        });
+    };
 
-	});
+    const initSwiperBookingPreview = function () {
+        if ($('#booking-preview').length) {
+            new Swiper('#booking-preview .swiper', {
+                speed: 1000,
+                autoplay: {
+                    delay: 8000,
+                    disableOnInteraction: false,
+                },
+                loop: 1,
+                navigation: {
+                    nextEl: '#booking-preview .swiper-button.next',
+                    prevEl: '#booking-preview .swiper-button.prev',
+                },
+            });
+        }
+    };
+
+    const showModalSearchUser = function () {
+        $('.checkout-guests .guest:not(:first)').on('click', function () {
+            $('#modal-add-user').modal('show');
+        });
+    };
+
+    const handleDatePicker = function (selectorId) {
+        if (!$(selectorId).length) return false;
+        $(selectorId).flatpickr({
+            dateFormat: 'd-m-Y',
+            defaultDate: 'today',
+            enable: [
+                {
+                    from: 'today',
+                    to: new Date().fp_incr(60)
+                }
+            ]
+        });
+    };
+
+
+
+
+    $(function () {
+		handleMenuMobile(windowWidth);
+        initSliderBanner();
+        initSliderTestimonials();
+        initSliderNews();
+        addPaddingSinglePage();
+        handleWindowResize();
+
+        // PAGE PRODUCT DETAIL
+        handleSlideProduct();
+        handleToggleCartList();
+
+        // PAGE BOOKING GOLF
+        initSwiperBookingPreview();
+        showModalSearchUser();
+
+        handleDatePicker('#date-picker');
+
+    });
 })(jQuery);
